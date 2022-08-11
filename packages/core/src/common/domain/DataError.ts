7e93a05a2196ed58a66212012c48775e603ee54a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-11 17:42:47 -0300

    feat(core): create common domain

---
+++ packages/core/src/common/domain/DataError.ts
@@ -0,0 +1,17 @@
+export interface ApiError {
+  kind: "ApiError";
+  error: string;
+  statusCode: number;
+  message: string;
+}
+export interface UnexpectedError {
+  kind: "UnexpectedError";
+  message: Error
+}
+export interface Unauthorized {
+  kind: "Unauthorized";
+}
+export interface NotFound {
+  kind: "NotFound";
+}
+export type DataError = ApiError | UnexpectedError | Unauthorized;
