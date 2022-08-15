d83bc33f7c0d3be0da6a8d0988456c8bc56f6845
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-15 19:28:21 -0300

    feat(app): create new component
    this componente is a list of products

---
+++ packages/vue-app/src/components/Products/OProductList.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { Product } from '@ecommerce/core/dist/products/domain/entites/Produto';
+import OProductCard from './OProductCard.vue';
+
+interface Props {
+  products: Product[];
+}
+defineProps<Props>();
+</script>
+<template>
+<div class="OProductList">
+  <div
+    v-for="product in products"
+    :key="product.id"
+    class="OProductList__Card"
+  >
+    <o-product-card
+      :image="product.image"
+      :alternative="product.title"
+      :title="product.title"
+      :description="product.description"
+      :price="product.price"
+    />
+  </div>
+</div>
+</template>
+<style lang="scss" scoped>
+.OProductList {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  column-gap: 2rem;
+  row-gap: 2rem;
+  margin: 0 auto;
+}
+</style>
