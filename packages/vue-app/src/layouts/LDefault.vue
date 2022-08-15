9bf629da926886becd3d38f2536f37cd05f873b0
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-15 19:32:07 -0300

    feat(app): create new layout
    this layout is the default layout of the application

---
+++ packages/vue-app/src/layouts/LDefault.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="LDefault">
+    <slot></slot>
+  </div>
+</template>
+<style lang="scss" scoped>
+.LDefault { 
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
