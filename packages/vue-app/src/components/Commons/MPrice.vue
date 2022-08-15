ee9b09a07e8dfb0f95a976f0eae6859e08d758b5
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-15 19:30:42 -0300

    feat(app): create new component
    this componente represent a generic price

---
+++ packages/vue-app/src/components/Commons/MPrice.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import { CurrencyCode, CurrencySymbol } from "@/type";
+
+interface Props {
+  value: number;
+  currency: CurrencyCode;
+  label: string;
+}
+const props = defineProps<Props>();
+const mapCurrencyCodeToSymbol: { [code in CurrencyCode]: CurrencySymbol } = {
+  "USD": "$",
+  "BRL": "R$",
+  "EUR": "€"
+}
+const getCurrencySimbol = computed(() =>  mapCurrencyCodeToSymbol[props.currency]);
+
+</script>
+<template>
+<div class="MPrice">
+  <span class="MPrice__Label">{{ label }}</span>
+  <div class="MPrice__Currency">
+    <span class="MPrice__Symbol">{{ getCurrencySimbol }}</span>
+    <span class="MPrice__Value">{{ value }}</span>
+  </div>
+</div>
+</template>
+<style lang="scss">
+
+</style>
