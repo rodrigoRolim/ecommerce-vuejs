0ed2f1a11d2eca1ad7563be681a67e7b454af75b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-15 19:31:33 -0300

    feat(app): create a new component
    this componente is the header of the products list

---
+++ packages/vue-app/src/components/Products/OProductHeader.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  amount: number;
+}
+defineProps<Props>();
+
+</script>
+<template>
+  <div class="OProductsHeader">
+    {{ amount }}
+  </div>
+</template>
+<style lang="scss" scoped></style>
