cc6e9af304f43c2aecda31912e8034e309ee8e09
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-15 19:32:44 -0300

    feat(app): create new template
    this is a template of the product page

---
+++ packages/vue-app/src/templates/TProducts.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="TProducts">
+    <div class="TProducts__Header">
+      <slot name="Header"></slot>
+    </div>
+    <slot name="navigation"></slot>
+    <div class="TProducts__Content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.TProducts {
+  min-width: 1280px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.TProducts__Content {
+  align-self: center;
+}
+</style>
