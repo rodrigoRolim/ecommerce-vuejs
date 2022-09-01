64ebdf701d216e30397e07f76b6ff3afa58029cb
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-08-31 22:56:54 -0300

    feat(componente): apply input count component to product card

--- packages/vue-app/src/components/Products/OProductCard.vue
+++ packages/vue-app/src/components/Products/OProductCard.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import MPrice from "@/components/Commons/MPrice.vue";
+import { MPrice, MInputCount } from "@/components/Commons";
+
 import { computed } from "vue";
 
 interface Props {
@@ -20,10 +21,13 @@ const truncatedTitle = computed(() => {
 });
 </script>
 <template>
-<div class="MProductCard card px-3 ">
+<div class="MProductCard card px-3 rounded-1">
+  <div class="MProductCard__Header">
+    
+  </div>
   <img :src="image" class="card-img-top p-0" :alt="alternative">
   <div class="MProductCard__Body card-body py-2">
-    <h6 class="card-title p-0 m-0 truncate-text">{{ title }}</h6>
+    <h6 class="card-title p-0 m-0 truncate-text fs-200">{{ title }}</h6>   
   </div>
   <div class="MProductCard__Price card-body">
     <m-price 
@@ -31,10 +35,23 @@ const truncatedTitle = computed(() => {
       currency="BRL"
       :value="price"
     />
+    <m-input-count
+      :min="0"
+      :max="100"
+      class="MProductCard__InputCount mt--n20"
+    />
   </div>
 </div>
 </template>
 <style lang="scss" scoped>
+.card {
+  border: none;
+  border-radius: 0;
+  &:hover {
+    background-color: rgb(245, 245, 245);
+    cursor: pointer;
+  }
+}
 .MProductCard {
   width: 18rem;
   & img {
@@ -42,6 +59,15 @@ const truncatedTitle = computed(() => {
     height: 15rem;
     align-self: center;
   }
+}
+.MProductCard__Price {
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+}
+.MProductCard__InputCount {
+  max-width: 90px;
+  max-height: 30px;
 }
 .MProductCard__Body {
   width: 100%;
